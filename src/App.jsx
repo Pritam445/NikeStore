5222372a6a4aa3dd8aac23ecb14ceeab348c6f31
Pritam <pritamghanta445@gmail.com>
2025-09-01 00:33:27 +0530

    Updated App.jsx and fixed styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-amber-500 text-7xl'>App</div>
+    <div className='text-amber-500 text-7xl xl:text-8xl'>App</div>
   )
 }
 
